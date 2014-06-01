5ec85f1df05f37fba8b84c8672216d89960f74c5
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-01 20:16:29 -0300

    added projects first urls

--- exway/urls.py
+++ exway/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'exway.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'', include('exway.core.urls', namespace='core')),
     url(r'^admin/', include(admin.site.urls)),
 )
