f36011e8b79d92cf77d44f29107a209191d6b921
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-01 22:51:41 -0300

    changed jquery script, added angular route script and added ng-view

--- exway/static/js/app.js
+++ exway/static/js/app.js
@@ -1,9 +1,22 @@
 (function() {
-  var app = angular.module('exway', []).
+  var app = angular.module('exway', ['ngRoute']).
     config(function($httpProvider, $interpolateProvider) {
       $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
       $interpolateProvider.startSymbol('{$');
       $interpolateProvider.endSymbol('$}');
     });
+
+  app.config(function($routeProvider){
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/expenses.html',
+        controller: 'ExpensesController'
+      }).
+      otherwise({redirectTo: '/'});
+  });
+
+  app.controller('ExpensesController', function($scope){
+
+  });
 })();
