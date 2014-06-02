e778392a70e9b169e355f87b3e5769acc26222c0
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-01 22:53:48 -0300

    added urls and view for angular partials

--- exway/core/urls.py
+++ exway/core/urls.py
@@ -1,5 +1,14 @@
 from django.conf.urls import patterns, include, url
+from exway.core.views import PartialView
+
+partialpatterns = patterns('',
+    url(r'^expenses.html$',
+        PartialView.as_view(template_name='expenses.html')),
+    url(r'^reports.html$',
+        PartialView.as_view(template_name='reports.html'))
+)
 
 urlpatterns = patterns('exway.core.views',
     url(r'^$', 'home', name='home'),
+    url(r'^partials/', include(partialpatterns, namespace='partials')),
 )
--- exway/core/views.py
+++ exway/core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class PartialView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(PartialView, self).get_context_data(**kwargs)
+        return context
+
 
 def home(request):
     """ This view just returns the html for the draw page """
