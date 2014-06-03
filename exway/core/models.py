52e1ddd890ff9071aea2c38a6eac40f124755148
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-03 01:56:43 -0300

    first basic expenses model

--- exway/core/models.py
+++ exway/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Expense(models.Model):
+    description = models.CharField(max_length=256)
+    datetime = models.DatetimeField()
+    amount = models.DecimalField()
+    comment = models.CharField(max_length=100)
+
+    created_on = models.DatetimeField(auto_now_add=True)
