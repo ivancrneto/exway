e4371300af44c2351785cc0b074d98560efb053a
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-04 01:46:06 -0300

    changing model behavior to fully work with separates date and time

--- exway/core/models.py
+++ exway/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Expense(models.Model):
     description = models.CharField(max_length=256)
@@ -8,17 +9,38 @@ class Expense(models.Model):
 
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __init__(self, *args, **kwargs):
+        super(Expense, self).__init__(*args, **kwargs)
+
+        # in some situations the model is initialized using datetime and
+        # in other ones, using a date and a time. For the second case, we need
+        # to pick up the date and time arguments and set to the class
+        date = kwargs.pop('date', False)
+        if date:
+            self.date = date
+        time = kwargs.pop('time', False)
+        if time:
+            self.time = time
+
+    # these date and time properties are needed because the model uses
+    # datetime but the api separates in two fields: date and time.
     def get_date(self):
         return self.datetime.date()
 
     def set_date(self, date):
-        self.datetime = datetime.datetime.combine(date, self.datetime.time())
+        if self.datetime:
+            self.datetime = datetime.combine(date, self.datetime.time())
+        else:
+            self.datetime = datetime.combine(date, datetime.min.time())
 
     def get_time(self):
         return self.datetime.time()
 
     def set_time(self, time):
-        self.datetime = datetime.datetime.combine(self.datetime.date(), time)
+        if self.datetime:
+            self.datetime = datetime.combine(self.datetime.date(), time)
+        else:
+            self.datetime = datetime.combine(datetime.min.date(), time)
 
     date = property(get_date, set_date)
     time = property(get_time, set_time)
