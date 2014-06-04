e141a970a746943b0cd8968e4f64625e098a4ee4
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-04 01:14:38 -0300

    separating date and time in model and serializer

--- exway/core/models.py
+++ exway/core/models.py
@@ -7,3 +7,18 @@ class Expense(models.Model):
     comment = models.CharField(max_length=100)
 
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def get_date(self):
+        return self.datetime.date()
+
+    def set_date(self, date):
+        self.datetime = datetime.datetime.combine(date, self.datetime.time())
+
+    def get_time(self):
+        return self.datetime.time()
+
+    def set_time(self, time):
+        self.datetime = datetime.datetime.combine(self.datetime.date(), time)
+
+    date = property(get_date, set_date)
+    time = property(get_time, set_time)
--- exway/core/serializers.py
+++ exway/core/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import Expense
+import datetime
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
 
+    date = serializers.DateField()
+    time = serializers.TimeField()
+
     class Meta:
         model = Expense
-        fields = ('id', 'description', 'datetime', 'amount', 'comment',
+        fields = ('id', 'description', 'amount', 'date', 'time', 'comment',
                   'created_on')
         read_only_fields = ('id', 'created_on')
