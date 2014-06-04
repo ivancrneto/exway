81ab186011f84623ca734d1066faad05eb03ce27
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-03 22:12:20 -0300

    fix models

--- exway/core/models.py
+++ exway/core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Expense(models.Model):
     description = models.CharField(max_length=256)
-    datetime = models.DatetimeField()
-    amount = models.DecimalField()
+    datetime = models.DateTimeField()
+    amount = models.DecimalField(decimal_places=2, max_digits=100)
     comment = models.CharField(max_length=100)
 
-    created_on = models.DatetimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
