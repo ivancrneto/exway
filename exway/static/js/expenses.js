c2bb34c239794679cedee13dba27dbf04627fd41
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-05 14:55:18 -0300

    editing table row without calling api

--- exway/static/js/expenses.js
+++ exway/static/js/expenses.js
@@ -1,10 +1,19 @@
 (function(){
   var app = angular.module('expenses', []);
 
-  app.controller('ExpensesController', ['$http', function($http){
+  app.controller('ExpensesController', ['$log', '$http', function($log, $http){
     var expensesCtrl = this;
 
+    // this flag controls if the user is editing any table row
+    this.editing = false;
+
+    // this keeps the old expense for cases where the user cancels editing
+    this.oldExpense = {};
+
+    // currentExpense keeps the data from the adding form
     this.currentExpense = {};
+
+    // this is the list of all expenses that will come from server
     this.expenses = [];
 
     // get expenses data from api and fill expenses array
@@ -46,6 +55,25 @@
             //TODO: put message here
           }
         });
+    };
+
+    // makes the basic steps for editing an expense
+    this.startEditing = function(expense){
+      expensesCtrl.editing = expensesCtrl.expenses.indexOf(expense);
+      expensesCtrl.oldExpense = angular.copy(expense);
+    };
+
+    this.saveExpense = function(index) {
+      if(expensesCtrl.editing !== false){
+        expensesCtrl.editing = false;
+      }
+    };
+
+    this.cancelEdit = function(index){
+      if(expensesCtrl.editing !== false){
+        expensesCtrl.expenses[expensesCtrl.editing] = expensesCtrl.oldExpense;
+        expensesCtrl.editing = false;
+      }
     };
 
   }]);
