f350768d0ca4024a0a6886ef063d4fa9acca3a1a
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-06 04:39:56 -0300

    created basic login

--- exway/auth/views.py
+++ exway/auth/views.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.contrib.auth import login as _login
+from django.shortcuts import render, redirect
 
 def login(request):
     if request.method == 'GET':
+        return render(request, 'login.html', {})
+
+    try:
+        user = User.objects.get(username=request.POST['username'])
+        if user.check_password(request.POST['password']):
+            user.backend='django.contrib.auth.backends.ModelBackend'
+            _login(request, user)
+            request.session.set_expiry(60 * 60 * 1)  # 1 hour timeout
+            return redirect(request.GET.get('next', '/'))
+        else:
+            return render(request, 'login.html', {})
+    except User.DoesNotExist:
         return render(request, 'login.html', {})
