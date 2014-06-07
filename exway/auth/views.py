c4fa3d72acd9d0f769c232679184eb98b3643526
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-06 22:58:22 -0300

    login via email or username

--- exway/auth/views.py
+++ exway/auth/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import login as _login, logout as _logout
 from django.shortcuts import render, redirect
+from django.db.models import Q
 
 
 def login(request):
@@ -11,15 +12,17 @@ def login(request):
         return render(request, 'login.html', {})
 
     try:
-        user = User.objects.get(username=request.POST['username'])
-        if user.check_password(request.POST['password']):
-            user.backend='django.contrib.auth.backends.ModelBackend'
-            _login(request, user)
-            request.session.set_expiry(60 * 60 * 10000000)
-            return redirect(request.GET.get('next', '/'))
-        else:
-            return render(request, 'login.html', {})
+        user = User.objects.get(Q(username=request.POST['username']) |
+                                Q(email=request.POST['username']))
     except User.DoesNotExist:
+        return render(request, 'login.html', {})
+
+    if user.check_password(request.POST['password']):
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        _login(request, user)
+        request.session.set_expiry(60 * 60 * 10000000)
+        return redirect(request.GET.get('next', '/'))
+    else:
         return render(request, 'login.html', {})
 
 
