5713f8c6b7805d140e06fb0d73073609a02c5bb4
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-06 23:58:53 -0300

    added signup view

--- exway/auth/urls.py
+++ exway/auth/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('exway.auth.views',
-    # url(r'signup/$', 'signup', name='signup'),
+    url(r'signup/$', 'signup', name='signup'),
     url(r'login/$', 'login', name='login'),
     url(r'logout/$', 'logout', name='logout'),
 )
--- exway/auth/views.py
+++ exway/auth/views.py
@@ -1,7 +1,25 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import login as _login, logout as _logout
+from django.core.urlresolvers import reverse as r
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from exway.auth.forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {})
+
+    signup_form = SignUpForm(request.POST)
+    if signup_form.is_valid():
+        data = signup_form.cleaned_data
+        user = User(first_name=data['first_name'], last_name=data['last_name'],
+                    email=data['email'], username=data['username'],
+                    is_active=True)
+        user.set_password(data['password'])
+        user.save()
+
+    return redirect(r('core:home'))
 
 
 def login(request):
