95c11aa70493dfec3ff93657b4bdeb088cfc4edc
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-07 16:00:18 -0300

    pep8 urls

--- exway/core/urls.py
+++ exway/core/urls.py
@@ -3,14 +3,18 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from exway.core.views import PartialView, ExpensesList, ExpenseDetail
 
-partialpatterns = patterns('',
+partialpatterns = patterns(
+    '',
+
     url(r'^expenses.html$',
         login_required(PartialView.as_view(template_name='expenses.html'))),
     url(r'^reports.html$',
         login_required(PartialView.as_view(template_name='reports.html')))
 )
 
-urlpatterns = patterns('exway.core.views',
+urlpatterns = patterns(
+    'exway.core.views',
+
     url(r'^$', login_required(TemplateView.as_view(template_name="home.html")),
         name='home'),
     url(r'^api/expenses/$', ExpensesList.as_view()),
