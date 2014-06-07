0a782febea51bd24ce0fe43c3865c8b6ec1520a1
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-06 22:50:05 -0300

    login view redirect to home if user is already loggedin

--- exway/auth/views.py
+++ exway/auth/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 
 
 def login(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+
     if request.method == 'GET':
         return render(request, 'login.html', {})
 
