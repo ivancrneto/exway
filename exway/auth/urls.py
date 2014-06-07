44f4e8fc0bfcb7fc6a9423c6777be0161874fdb4
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-07 15:58:51 -0300

    added login using for rest framework browsable api

--- exway/auth/urls.py
+++ exway/auth/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
-urlpatterns = patterns('exway.auth.views',
+urlpatterns = patterns(
+    'exway.auth.views',
+
     url(r'signup/$', 'signup', name='signup'),
     url(r'login/$', 'login', name='login'),
     url(r'logout/$', 'logout', name='logout'),
+    url(r'api/', include('rest_framework.urls',
+                         namespace='rest_framework')),
 )
